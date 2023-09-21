05ed03f0533fea91e321ae8ee1acd8020b6898b0
César González <chesarglez429@gmail.com>
2023-09-21 12:19:55 -0600

    :fire: Remove stuff to show articles

--- modules/View.js
+++ modules/View.js
@@ -8,41 +8,6 @@ const View = (() => {
 
   const hideErrorMessage = () => showErrorMessage('')
 
-  const dateFormatter = new Intl.DateTimeFormat('es-MX', { dateStyle: 'full' })
-
-  const articleToHtmlElement = ({ title, pageid, snippet, size, date }) => {
-    const titleElement = document.createElement('a')
-    titleElement.setAttribute('href', `https://es.wikipedia.org/?curid=${pageid}`)
-    titleElement.setAttribute('class', 'article-title')
-    titleElement.innerText = title
-
-    const snippetElement = document.createElement('p')
-    snippetElement.setAttribute('class', 'article-snippet')
-    snippetElement.innerHTML = snippet
-
-    const dateElement = document.createElement('p')
-    dateElement.setAttribute('class', 'article-metadata')
-    dateElement.innerText = `Última modificación: ${dateFormatter.format(date)}`
-
-    const sizeElement = document.createElement('p')
-    sizeElement.setAttribute('class', 'article-metadata')
-    sizeElement.innerText = `${size} Bytes`
-
-    const containerElement = document.createElement('div')
-    containerElement.appendChild(titleElement)
-    containerElement.appendChild(snippetElement)
-    containerElement.appendChild(dateElement)
-    containerElement.appendChild(sizeElement)
-    return containerElement
-  }
-
-
-  const showArticles = articles => {
-    const articleElements = articles.map(articleToHtmlElement)
-    const articlesContainerElement = document.getElementById('articles')
-    articlesContainerElement.replaceChildren(...articleElements)
-  }
-
 
   const columnNameToThElement = columnName => {
     const thElement = document.createElement('th')
@@ -110,7 +75,6 @@ const View = (() => {
   return Object.freeze({
     showErrorMessage,
     hideErrorMessage,
-    showArticles,
     showTables,
   })
 })()
