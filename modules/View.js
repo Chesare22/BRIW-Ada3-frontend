3ad5244ef817a35ec7b163f120568bb8e1da9b14
César González <chesarglez429@gmail.com>
2023-09-21 13:05:56 -0600

    :sparkles: Show the titles of the tables

--- modules/View.js
+++ modules/View.js
@@ -64,11 +64,29 @@ const View = (() => {
     return tableElement
   }
 
+  function interleave(arr, arr2) {
+    const newArr = []
+    for (let i = 0; i < arr.length; i++) {
+      newArr.push(arr[i], arr2[i])
+    }
+
+    return newArr
+  }
+
+
+  const getHtmlTableTitles = ({ table_name }) => {
+    const h2Element = document.createElement('h2')
+    h2Element.setAttribute('class', 'table-title')
+    h2Element.innerText = table_name
+    return h2Element
+  }
+
 
   const showTables = tables => {
+    const htmlTableTitles = tables.map(getHtmlTableTitles)
     const htmlTables = tables.map(tableToHtmlElement)
     const tablesContainer = document.getElementById('tables')
-    tablesContainer.replaceChildren(...htmlTables)
+    tablesContainer.replaceChildren(...interleave(htmlTableTitles, htmlTables))
   }
 
 
